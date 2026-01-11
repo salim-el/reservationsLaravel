cd24e91cb619339fc4828ee02a6f69cd73e779be
Salim EL GHALBZOURI <elg.salim@gmail.com>
2026-01-11 19:16:08 +0100

    Add Breeze auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArtistController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/artist', [ArtistController::class, 'index'])->name('artist.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/artist/{id}', [ArtistController::class, 'show'])
-    ->where('id', '[0-9]+')
-    ->name('artist.show');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/artist/edit/{id}', [ArtistController::class, 'edit'])
-    ->where('id', '[0-9]+')
-    ->name('artist.edit');
-
-Route::put('/artist/{id}', [ArtistController::class, 'update'])
-    ->where('id', '[0-9]+')
-    ->name('artist.update');
-
-Route::get('/artist/create', [ArtistController::class, 'create'])
-    ->name('artist.create');
-
-Route::post('/artist', [ArtistController::class, 'store'])
-    ->name('artist.store');
-
-Route::delete('/artist/{id}', [ArtistController::class, 'destroy'])
-    ->where('id', '[0-9]+')
-    ->name('artist.delete');
-
-
+require __DIR__.'/auth.php';
