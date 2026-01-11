339ca8758f8028a813c1565d0addc55a7fc22551
Salim EL GHALBZOURI <elg.salim@gmail.com>
2026-01-11 19:24:37 +0100

    Update base config and layout

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -2,83 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Artist;
 use Illuminate\Http\Request;
+use App\Models\Artist; 
 
 class ArtistController extends Controller
 {
-    public function index()
-    {
-        $artists = Artist::all();
-        $resource = 'artistes';
-
-        return view('artist.index', [
-            'artists' => $artists,
-            'resource' => $resource,
-        ]);
-    }
-
-    public function show($id)
-    {
-        $artist = Artist::find($id);
-
-        return view('artist.show', [
-            'artist' => $artist,
-        ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('artist.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'firstname' => 'required|max:60',
-            'lastname'  => 'required|max:60',
-        ]);
-
-        $artist = new Artist();
-        $artist->firstname = $validated['firstname'];
-        $artist->lastname  = $validated['lastname'];
-        $artist->save();
-
-        return redirect()->route('artist.index');
-    }
-
-    public function edit($id)
-    {
-        $artist = Artist::find($id);
-
-        return view('artist.edit', [
-            'artist' => $artist,
-        ]);
-    }
-
-    public function update(Request $request, $id)
-    {
-        $validated = $request->validate([
-            'firstname' => 'required|max:60',
-            'lastname'  => 'required|max:60',
-        ]);
-
-        $artist = Artist::find($id);
-        $artist->firstname = $validated['firstname'];
-        $artist->lastname  = $validated['lastname'];
-        $artist->save();
-
-        return view('artist.show', [
-            'artist' => $artist,
-        ]);
-    }
-
     /**
      * Delete the specified resource.
      *
