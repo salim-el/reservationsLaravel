f94d45249a8a92f8b8b8a55a3e548b12ea43fce3
Salim EL GHALBZOURI <elg.salim@gmail.com>
2026-01-14 18:32:19 +0100

    fix(auth): restore dashboard & profile routes (Breeze)

--- routes/web.php
+++ routes/web.php
@@ -2,49 +2,66 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\TypeController;
-use App\Http\Controllers\PriceController;
-use App\Http\Controllers\LocalityController;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\LocationController;
-use App\Http\Controllers\ShowController;
-use App\Http\Controllers\RepresentationController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\ShowController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\RepresentationController;
+use App\Http\Controllers\LocalityController;
 
-// Type
-Route::get('/type', [TypeController::class, 'index'])->name('type.index');
-Route::get('/type/{id}', [TypeController::class, 'show'])->where('id', '[0-9]+')->name('type.show');
-
-// Price
-Route::get('/price', [PriceController::class, 'index'])->name('price.index');
-Route::get('/price/{id}', [PriceController::class, 'show'])->where('id', '[0-9]+')->name('price.show');
-
-// Locality
 Route::get('/locality', [LocalityController::class, 'index'])->name('locality.index');
-Route::get('/locality/{postal_code}', [LocalityController::class, 'show'])->where('postal_code', '[0-9]+')->name('locality.show');
+Route::get('/locality/{id}', [LocalityController::class, 'show'])
+    ->where('id','[0-9]+')->name('locality.show');
 
-// Role
-Route::get('/role', [RoleController::class, 'index'])->name('role.index');
-Route::get('/role/{id}', [RoleController::class, 'show'])->where('id', '[0-9]+')->name('role.show');
 
-// Location
-Route::get('/location', [LocationController::class, 'index'])->name('location.index');
-Route::get('/location/{id}', [LocationController::class, 'show'])->where('id', '[0-9]+')->name('location.show');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Show
-Route::get('/show', [ShowController::class, 'index'])->name('show.index');
-Route::get('/show/{id}', [ShowController::class, 'show'])->where('id', '[0-9]+')->name('show.show');
+// Dashboard Breeze
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-// Representation
-Route::get('/representation', [RepresentationController::class, 'index'])->name('representation.index');
-Route::get('/representation/{id}', [RepresentationController::class, 'show'])->where('id', '[0-9]+')->name('representation.show');
 
-// Artist
-// Artist
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
 Route::get('/artist', [ArtistController::class, 'index'])->name('artist.index');
-Route::get('/artist/create', [ArtistController::class, 'create'])->name('artist.create');
-Route::post('/artist', [ArtistController::class, 'store'])->name('artist.store');
-Route::get('/artist/{id}', [ArtistController::class, 'show'])->where('id', '[0-9]+')->name('artist.show');
-Route::get('/artist/{id}/edit', [ArtistController::class, 'edit'])->where('id', '[0-9]+')->name('artist.edit');
-Route::put('/artist/{id}', [ArtistController::class, 'update'])->where('id', '[0-9]+')->name('artist.update');
-Route::delete('/artist/{id}', [ArtistController::class, 'destroy'])->where('id', '[0-9]+')->name('artist.delete');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/artist/create', [ArtistController::class, 'create'])->name('artist.create');
+    Route::post('/artist', [ArtistController::class, 'store'])->name('artist.store');
+
+    Route::get('/artist/edit/{id}', [ArtistController::class, 'edit'])
+        ->where('id', '[0-9]+')->name('artist.edit');
+    Route::put('/artist/{id}', [ArtistController::class, 'update'])
+        ->where('id', '[0-9]+')->name('artist.update');
+    Route::delete('/artist/{id}', [ArtistController::class, 'destroy'])
+        ->where('id', '[0-9]+')->name('artist.delete');
+});
+
+Route::get('/artist/{id}', [ArtistController::class, 'show'])
+    ->where('id', '[0-9]+')->name('artist.show');
+
+// SHOW
+Route::get('/show', [ShowController::class, 'index'])->name('show.index');
+Route::get('/show/{id}', [ShowController::class, 'show'])
+    ->where('id', '[0-9]+')->name('show.show');
+
+// LOCATION
+Route::get('/location', [LocationController::class, 'index'])->name('location.index');
+Route::get('/location/{id}', [LocationController::class, 'show'])
+    ->where('id', '[0-9]+')->name('location.show');
+
+// REPRESENTATION
+Route::get('/representation', [RepresentationController::class, 'index'])->name('representation.index');
+Route::get('/representation/{id}', [RepresentationController::class, 'show'])
+    ->where('id', '[0-9]+')->name('representation.show');
+
+// Breeze auth routes (/login, /register, /logout...)
+require __DIR__.'/auth.php';
